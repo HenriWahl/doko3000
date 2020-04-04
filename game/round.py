b3ebd335280f22aad10ff62e19efd9b8d4bd39a0
Henri Wahl <h.wahl@ifw-dresden.de>
2020-04-05 00:12:12 +0200

    starting organizing a game

--- game/round.py
+++ game/round.py
@@ -2,3 +2,16 @@ class Round:
     """
     one round
     """
+
+    def shuffle(self):
+        """
+        shuffle deck
+        """
+        pass
+
+
+    def deal_out(self):
+        """
+        deal out cards
+        """
+        pass
