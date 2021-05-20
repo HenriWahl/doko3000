5ef710b97c28c5b0982ecf3822d2b509db42772b
Henri Wahl <h.wahl@ifw-dresden.de>
2021-05-20 15:00:03 +0200

    debug switch_to_started_table part II

--- doko3000/__init__.py
+++ doko3000/__init__.py
@@ -369,6 +369,7 @@ def setup_table(msg):
                 # get current URL
                 url_path = f'/{request.referrer.replace(request.url_root, "", 1)}'
                 # when being called from start page aka '/' just enter table if already being a member
+                print(url_path)
                 if url_path == '/':
                     socketio.emit('redirect-to-path',
                                   {'path': f'/table/{table.id}'},
